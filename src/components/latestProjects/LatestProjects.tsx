aa4c81bdf67c36bd251121903735fad75352f5d2
Ahmd-Khaled <ahmd.khaldd@gmail.com>
2024-01-18 09:54:18 +0200

    first commit

---
+++ src/components/latestProjects/LatestProjects.tsx
@@ -0,0 +1,78 @@
+import { PiPlusBold } from "react-icons/pi";
+import { Col, Container, Row } from "react-bootstrap";
+
+import styles from "./styles.module.css";
+
+const LatestProjects = () => {
+  return (
+    <div className={styles.latestProjects}>
+      <Container>
+        <Row>
+          <div className={styles.container}>
+            <div className={styles.top}>
+              <p className={styles.smTitle}>LATEST PROJECTS</p>
+              <h3 className={styles.title}>
+                EXCELLENCE DRIVEN FOR GETTING
+                <br />
+                UNRIVALED RESULTS.
+              </h3>
+            </div>
+            <ul className={styles.projectsList}>
+              <ProjectItem
+                img="proj-icon-01.svg"
+                num={3526}
+                text="Tons Of Customers"
+              />
+              <ProjectItem
+                img="proj-icon-02.svg"
+                num={3024}
+                text="Compleated Project"
+              />
+              <ProjectItem
+                img="proj-icon-03.svg"
+                num={325}
+                text="Team Members"
+              />
+              <ProjectItem
+                img="proj-icon-04.svg"
+                num={56}
+                text="Upcoming Projects"
+              />
+            </ul>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default LatestProjects;
+// ---------------------------------------------------------
+// var process: NodeJS.Process
+// var env: NodeJS.ProcessEnv;
+
+
+type projectType = {
+  img: string;
+  num: number;
+  text: string;
+};
+
+// : projectType
+
+export const ProjectItem = ({ img, num, text }: projectType) => {
+  return (
+    <li className={styles.projItem}>
+      <div className={styles.itemWrapper}>
+        <div className={styles.image}>
+          <img src={`/images/${img}`} alt="icon" />
+        </div>
+        <h2 className={styles.projNum}>
+          <span>{num}</span>
+          <PiPlusBold />
+        </h2>
+        <p className={styles.projText}>{text}</p>
+      </div>
+    </li>
+  );
+};
---
+++ src/pages/Home.tsx
@@ -0,0 +1,54 @@
+import { Col, Container, Row } from "react-bootstrap";
+import Banner from "../components/banner/Banner";
+import Helmet from "../components/helmet/Helmet";
+import styles from "../styles/home.module.css";
+import Services from "./Services";
+import Portfolio from "./Portfolio";
+import About from "./About";
+import LatestProjects from "../components/latestProjects/LatestProjects";
+import GetInTouch from "../components/getInTouch/GetInTouch";
+import ContactUs from "../components/contactUs/ContactUs";
+import Blog from "../components/blog/Blog";
+
+const Home = () => {
+  return (
+    <Helmet title="Home">
+      <div className="banner-main-area">
+        <Banner />
+      </div>
+
+      <section className={styles.services}>
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center mb-5">
+              <span className="span-title">Our Services</span>
+              <h3 className="section-title">Quality Servicing Opportunity</h3>
+            </Col>
+            <Services />
+          </Row>
+        </Container>
+      </section>
+      <About />
+      <Portfolio />
+
+
+
+      {/* ------------Ahmed Part-------------------- */}
+      
+      {/* <LatestProjects />    */}
+      <Container>
+          <Row>
+            <Blog />
+          </Row>
+        </Container>  
+      <ContactUs />
+      <Container>
+          <Row>
+            <GetInTouch />
+          </Row>
+        </Container>
+    </Helmet>
+  );
+};
+
+export default Home;
